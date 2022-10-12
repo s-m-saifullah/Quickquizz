45ea7f8e6691dbc6158f23632f67b37a5120a706
shaquille19 <shakil.m.19@gmailcom>
2022-10-12 12:02:38 +0600

    remove unused daisyui themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,32 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "coffee",
-    ],
+    themes: ["luxury"],
   },
   theme: {
     extend: {
